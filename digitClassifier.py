282a50f92488aef36d7b2db961552c676c62746b
sgrandchant1 <sgrandchant1@gmail.com>
2024-05-15 20:10:02 -0400

    adding dataset and imports to the code

--- digitClassifier.py
+++ digitClassifier.py
@@ -3,3 +3,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+filePath = '/Users/santiagodegrandchant/Desktop/AI_Projects/handwrittenDigitClassifier/train.csv'
+data = pd.read_csv(filePath)
+#print(data.head())
+
+data = np.array(data)
+
