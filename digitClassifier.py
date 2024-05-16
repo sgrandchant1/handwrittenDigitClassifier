4ffbe8ecd8b705a14a1669a35114009300ee3468
sgrandchant1 <sgrandchant1@gmail.com>
2024-05-15 20:52:00 -0400

    adding both test and train data sets into variables. also getting the labels both from the x axis and y axis for both.

--- digitClassifier.py
+++ digitClassifier.py
@@ -1,12 +1,27 @@
-# I will be using a database that has already convirted hundreds of handwritten numbers 
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-filePath = '/Users/santiagodegrandchant/Desktop/AI_Projects/handwrittenDigitClassifier/train.csv'
-data = pd.read_csv(filePath)
-#print(data.head())
+train = '/Users/santiagodegrandchant/Desktop/AI_Projects/handwrittenDigitClassifier/train.csv'
+test = '/Users/santiagodegrandchant/Desktop/AI_Projects/handwrittenDigitClassifier/test.csv'
+data_train = pd.read_csv(train)
+data_test = pd.read_csv(test)
 
-data = np.array(data)
+data_train.head()
+data_test.head()
+
+data_train = np.array(data_train)
+data_test = np.array(data_test)
+
+np.random.shuffle(data_train)
+np.random.shuffle(data_test)
+
+data_train = data_train.T
+data_test = data_test.T
+
+Y_train = data_train[0]
+X_train = data_train[1:]
+
+Y_test = data_test[0]
+X_test = data_test[1:]
 
